b697f0a957d5a24bd4b2faf64221932950c22bed
YI GUO <guoyi1023@gmail.com>
2016-09-29 14:21:17 +0800

    The first commit. IF statement

--- GeoQuiz/QuizViewController.swift
+++ GeoQuiz/QuizViewController.swift
@@ -37,5 +37,12 @@ class QuizViewController: UIViewController {
     @IBAction func flagButtonPressed(sender: UIButton) {
 
         // TODO: Add code to display a message to the user telling them whether or not they guessed correctly.
+        // TODO: Add code to display a message to the user telling them whether or not they guessed correctly.
+        if sender.tag == correctButtonTag {
+            displayAlert("Correct!", messageText: "Right on! ")
+        } else{
+            displayAlert("Incorrect!", messageText: "Nope. Try again")
+        }
+
     }
 }
